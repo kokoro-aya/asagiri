1b73e6286e4d54a704c06c8c68fff4f6f3d03d7f
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2023-11-24 23:15:08 +0100

    Basic create new company page implementation and triggered some details in both views, the navigation is not available yet

---
+++ asagiri/Views/CreateNewCompanyView.swift
@@ -0,0 +1,74 @@
+//
+//  CreateNewCompanyView.swift
+//  asagiri
+//
+//  Created by irony on 24/11/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+struct CreateNewCompanyView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State var name: String = ""
+    
+    @State var website: String = ""
+    
+    var onCompletion: (_ company: Company) -> ()
+    
+    init () {
+        self.onCompletion = { _ in }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Create a new company")
+                        .font(.caption)
+                    Spacer()
+                }
+                Divider()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Name")
+                            .font(.title3)
+                        Spacer()
+                    }
+                    TextField("Company name", text: $name)
+                        .padding([.leading], 6)
+                    HStack {
+                        Text("Website")
+                            .font(.title3)
+                        Spacer()
+                    }
+                    TextField("Website", text: $website)
+                        .padding([.leading], 6)
+                }
+                Spacer()
+                Divider()
+                Button {
+                    onCompletion(Company(name: name, website: website))
+                    presentationMode.wrappedValue.dismiss()
+                    
+                } label: {
+                    Label("Save", systemImage: "paperplane.fill")
+                        .padding(12)
+                }
+                
+            }.padding(16)
+        }
+    }
+}
+
+#Preview {
+    MainActor.assumeIsolated {
+        var previewContainer: ModelContainer = initializePreviewContainer()
+        
+        
+        return CreateNewCompanyView()
+            .modelContainer(previewContainer)
+    }
+}
--- asagiri/Views/CreateNewJDView.swift
+++ asagiri/Views/CreateNewJDView.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 struct CreateNewJDView: View {
     
-    init(pendingJD: JobDescription, allJobTypes: [CareerType]) {
+    init(pendingJD: JobDescription, allJobTypes: [CareerType], allCompanies: [Company]) {
         self.title = pendingJD.title
         self.company = pendingJD.company
         self.type = pendingJD.type
@@ -20,13 +20,17 @@ struct CreateNewJDView: View {
         self.complementary = pendingJD.complementary
         
         self.allJobTypes = allJobTypes
+        self.allCompanies = allCompanies
     }
     
-    init(allJobTypes: [CareerType]) {
+    init(allJobTypes: [CareerType], allCompanies: [Company]) {
         self.allJobTypes = allJobTypes
+        self.allCompanies = allCompanies
     }
     
     let allJobTypes: [CareerType]
+    
+    let allCompanies: [Company]
     
     @State var expanded: (Bool, Bool, Bool, Bool) = (true, false, false, false)
     
@@ -42,98 +46,116 @@ struct CreateNewJDView: View {
     
     @State var complementary: String = ""
     
-    var company: Company? = nil
+    @State var company: Company? = nil
+    
+    var incomplete: Bool {
+        return title.count > 0
+            && company != nil
+    }
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Create a new job description")
-                    .font(.caption)
-                Spacer()
-            }
-            Divider()
-            ScrollView {
-                VStack(alignment: .leading) {
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading) {
-                            Text("Job title")
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Create a new job description")
+                        .font(.caption)
+                    Spacer()
+                }
+                Divider()
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .top) {
+                            VStack(alignment: .leading) {
+                                Text("Job title")
+                                    .font(.title3)
+                                TextField("Title", text: $title)
+                                    .padding([.leading], 6)
+                            }
+                            Spacer()
+                            
+                            JobTypeDropdownSelector(allJobTypes: allJobTypes, jobType: $type)
+                        }
+                        HStack(alignment: .top) {
+                            VStack(alignment: .leading) {
+                                Text("Company")
+                                    .font(.title3)
+                            }
+                            Spacer()
+                            
+                            CompanyDropdownSelector(allCompanies: allCompanies, company: $company)
+                        }
+                        Divider()
+                        HStack {
+                            Text("Introduction")
                                 .font(.title3)
-                            TextField("Title", text: $title)
-                                .padding([.leading], 6)
+                            Spacer()
+                            CollapseToggle(toggled: $expanded.0) {
+                                expanded.0 = !(expanded.0)
+                            }
                         }
-                        Spacer()
-                        
-                        JobTypeDropdownSelector(allJobTypes: allJobTypes, jobType: $type)
-                    }
-                    HStack {
-                        Text("Introduction")
-                            .font(.title3)
-                        Spacer()
-                        CollapseToggle(toggled: $expanded.0) {
-                            expanded.0 = !(expanded.0)
+                        if (expanded.0) {
+                            CustomTextEditor(text: $intro, height: 60)
+                        } else {
+                            Spacer()
+                                .frame(height: 16)
                         }
-                    }
-                    if (expanded.0) {
-                        CustomTextEditor(text: $intro, height: 60)
-                    } else {
-                        Spacer()
-                            .frame(height: 16)
-                    }
-                    Divider()
-                    HStack {
-                        Text("Company Intro")
-                            .font(.title3)
-                        Spacer()
-                        CollapseToggle(toggled: $expanded.1) {
-                            expanded.1 = !(expanded.1)
+                        Divider()
+                        HStack {
+                            Text("Company Intro")
+                                .font(.title3)
+                            Spacer()
+                            CollapseToggle(toggled: $expanded.1) {
+                                expanded.1 = !(expanded.1)
+                            }
                         }
-                    }
-                    if (expanded.1) {
-                        CustomTextEditor(text: $companyIntro, height: 60)
-                    } else {
-                        Spacer()
-                            .frame(height: 16)
-                    }
-                    Divider()
-                    HStack {
-                        Text("Responsibilities")
-                            .font(.title3)
-                        Spacer()
-                        CollapseToggle(toggled: $expanded.2) {
-                            expanded.2 = !(expanded.2)
+                        if (expanded.1) {
+                            CustomTextEditor(text: $companyIntro, height: 60)
+                        } else {
+                            Spacer()
+                                .frame(height: 16)
                         }
-                    }
-                    if (expanded.2) {
-                        CustomTextEditor(text: $responsibilities, height: 60)
-                    } else {
-                        Spacer()
-                            .frame(height: 16)
-                    }
-                    Divider()
-                    HStack {
-                        Text("Complementary")
-                            .font(.title3)
-                        Spacer()
-                        CollapseToggle(toggled: $expanded.3) {
-                            expanded.3 = !(expanded.3)
+                        Divider()
+                        HStack {
+                            Text("Responsibilities")
+                                .font(.title3)
+                            Spacer()
+                            CollapseToggle(toggled: $expanded.2) {
+                                expanded.2 = !(expanded.2)
+                            }
                         }
-                    }
-                    if (expanded.3) {
-                        CustomTextEditor(text: $complementary, height: 60)
-                    } else {
-                        Spacer()
-                            .frame(height: 16)
+                        if (expanded.2) {
+                            CustomTextEditor(text: $responsibilities, height: 60)
+                        } else {
+                            Spacer()
+                                .frame(height: 16)
+                        }
+                        Divider()
+                        HStack {
+                            Text("Complementary")
+                                .font(.title3)
+                            Spacer()
+                            CollapseToggle(toggled: $expanded.3) {
+                                expanded.3 = !(expanded.3)
+                            }
+                        }
+                        if (expanded.3) {
+                            CustomTextEditor(text: $complementary, height: 60)
+                        } else {
+                            Spacer()
+                                .frame(height: 16)
+                        }
                     }
                 }
-            }
-            Divider()
-            Button {
-//                NavigationLink(destination: <#T##() -> View#>, label: <#T##() -> View#>)
-            } label: {
-                Label("Save", systemImage: "paperplane.fill")
-                    .padding(12)
-            }
-        }.padding(16)
+                Divider()
+                Button {
+                    //                NavigationLink(destination: <#T##() -> View#>, label: <#T##() -> View#>)
+                } label: {
+                    Label("Save", systemImage: "paperplane.fill")
+                        .padding(12)
+                }
+                .disabled(!incomplete)
+            }.padding(16)
+        }
     }
 }
 
@@ -141,13 +163,21 @@ struct CreateNewJDView: View {
     MainActor.assumeIsolated {
         var previewContainer: ModelContainer = initializePreviewContainer()
         
+        let companies = [
+            Company(name: "Company 1", website: "com.1"),
+            Company(name: "Company 2", website: "com.2"),
+            Company(name: "Company 3", website: "com.3"),
+            Company(name: "Company 4", website: "com.4"),
+            Company(name: "Company 5", website: "com.5"),
+        ]
         
         return CreateNewJDView(
             allJobTypes: [
                 CareerType(name: "Front-end"),
                 CareerType(name: "Back-end"),
                 CareerType(name: "Fullstack"),
-            ]
+            ],
+            allCompanies: companies
         )
         .modelContainer(previewContainer)
     }
@@ -175,7 +205,38 @@ struct JobTypeDropdownSelector : View {
                 Label("Delete", systemImage: "trash")
             }
         } label: {
-            Label("Switch Type", systemImage: "ellipsis.circle")
+            Label(jobType?.name ?? "Job Type", systemImage: "bag.fill")
+                .foregroundColor(jobType == nil ? .blue : .black)
+        }
+    }
+}
+
+struct CompanyDropdownSelector : View {
+    
+    let allCompanies: [Company]
+    
+    @Binding var company: Company?
+    
+    
+    var body: some View {
+        Menu {
+            ForEach(allCompanies) { com in
+                Button(com.name) {
+                    self.company = com
+                }
+            }
+            Divider()
+            NavigationLink(destination: CreateNewCompanyView()) {
+                Label("Create new one", systemImage: "plus")
+            }
+            Button(role: .destructive) {
+                self.company = nil
+            } label: {
+                Label("Delete", systemImage: "trash")
+            }
+        } label: {
+            Label(company?.name ?? "Select one", systemImage: "building.2.fill")
+                .foregroundColor(company == nil ? .blue : .black)
         }
     }
 }
