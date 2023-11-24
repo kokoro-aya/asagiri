9afec65a0d186a1af1e9f4bdc72c1d59d868bc23
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2023-11-24 19:19:59 +0100

    Remove models in main app starter since this will break everything on preview, to be solved later

---
+++ asagiri/Views/Parts/ApplicationCard.swift
@@ -0,0 +1,18 @@
+//
+//  ApplicationCard.swift
+//  asagiri
+//
+//  Created by irony on 24/11/2023.
+//
+
+import SwiftUI
+
+struct ApplicationCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApplicationCard()
+}
--- asagiri/asagiriApp.swift
+++ asagiri/asagiriApp.swift
@@ -12,22 +12,14 @@ import SwiftData
 struct asagiriApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
-            
-            Application.self,
-            Company.self,
-            Resume.self,
-            CoverLetter.self,
-            JobDescription.self,
-            CareerType.self,
-            Event.self
+            Item.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
-            fatalError("Could not create ModelContainer: \(error)")
+            fatalError("Could not create ModelContainer: \(error.localizedDescription)")
         }
     }()
 
