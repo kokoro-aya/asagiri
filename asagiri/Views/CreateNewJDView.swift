5042471faccad1c08daee5da22d008a0cac3ceb2
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2023-11-24 22:23:17 +0100

    Basic create new JD view implementation

--- asagiri/Views/CreateNewJDView.swift
+++ asagiri/Views/CreateNewJDView.swift
@@ -26,13 +26,13 @@ struct CreateNewJDView: View {
         self.allJobTypes = allJobTypes
     }
     
+    let allJobTypes: [CareerType]
+    
+    @State var expanded: (Bool, Bool, Bool, Bool) = (true, false, false, false)
+    
     @State var title: String = ""
     
-    var company: Company? = nil
-    
     @State var type: CareerType? = nil
-    
-    let allJobTypes: [CareerType]
     
     @State var intro: String = ""
     
@@ -42,29 +42,98 @@ struct CreateNewJDView: View {
     
     @State var complementary: String = ""
     
+    var company: Company? = nil
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack {
             HStack {
-                VStack {
-                    Text("Job title")
-                        .font(.subheadline)
-                    TextField("Title", text: $title)
-                }
+                Text("Create a new job description")
+                    .font(.caption)
                 Spacer()
-            
-                JobTypeDropdownSelector(allJobTypes: allJobTypes, jobType: $type)
             }
-            Text("Type: \(type?.name ?? "No Type")")
-            Text("Job title")
-                .font(.subheadline)
-            TextField("Title", text: $title)
-            Text("Job title")
-                .font(.subheadline)
-            TextField("Title", text: $title)
-            Text("Job title")
-                .font(.subheadline)
-            TextField("Title", text: $title)
-        }
+            Divider()
+            ScrollView {
+                VStack(alignment: .leading) {
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading) {
+                            Text("Job title")
+                                .font(.title3)
+                            TextField("Title", text: $title)
+                                .padding([.leading], 6)
+                        }
+                        Spacer()
+                        
+                        JobTypeDropdownSelector(allJobTypes: allJobTypes, jobType: $type)
+                    }
+                    HStack {
+                        Text("Introduction")
+                            .font(.title3)
+                        Spacer()
+                        CollapseToggle(toggled: $expanded.0) {
+                            expanded.0 = !(expanded.0)
+                        }
+                    }
+                    if (expanded.0) {
+                        CustomTextEditor(text: $intro, height: 60)
+                    } else {
+                        Spacer()
+                            .frame(height: 16)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Company Intro")
+                            .font(.title3)
+                        Spacer()
+                        CollapseToggle(toggled: $expanded.1) {
+                            expanded.1 = !(expanded.1)
+                        }
+                    }
+                    if (expanded.1) {
+                        CustomTextEditor(text: $companyIntro, height: 60)
+                    } else {
+                        Spacer()
+                            .frame(height: 16)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Responsibilities")
+                            .font(.title3)
+                        Spacer()
+                        CollapseToggle(toggled: $expanded.2) {
+                            expanded.2 = !(expanded.2)
+                        }
+                    }
+                    if (expanded.2) {
+                        CustomTextEditor(text: $responsibilities, height: 60)
+                    } else {
+                        Spacer()
+                            .frame(height: 16)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Complementary")
+                            .font(.title3)
+                        Spacer()
+                        CollapseToggle(toggled: $expanded.3) {
+                            expanded.3 = !(expanded.3)
+                        }
+                    }
+                    if (expanded.3) {
+                        CustomTextEditor(text: $complementary, height: 60)
+                    } else {
+                        Spacer()
+                            .frame(height: 16)
+                    }
+                }
+            }
+            Divider()
+            Button {
+//                NavigationLink(destination: <#T##() -> View#>, label: <#T##() -> View#>)
+            } label: {
+                Label("Save", systemImage: "paperplane.fill")
+                    .padding(12)
+            }
+        }.padding(16)
     }
 }
 
@@ -106,7 +175,49 @@ struct JobTypeDropdownSelector : View {
                 Label("Delete", systemImage: "trash")
             }
         } label: {
-            Label("Jobs", systemImage: "ellipsis.circle")
+            Label("Switch Type", systemImage: "ellipsis.circle")
         }
+    }
+}
+
+struct CustomTextEditor : View {
+    
+    @Binding var text: String
+    
+    let height: Int
+    
+    let placeholder: String = "Enter your description here ..."
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            TextEditor(text: $text)
+                .frame(height: 160)
+            if (text == "") {
+                Text(placeholder).fontWeight(.light).foregroundColor(.black.opacity(0.25))
+                    .padding(8)
+                    .padding([.leading], 0)
+                    .allowsHitTesting(false)
+            }
+        }
+    }
+}
+
+
+struct CollapseToggle : View {
+    
+    @Binding var toggled: Bool
+    
+    let onPress: () -> ()
+    
+    var body: some View {
+        Button (action: onPress) {
+            if (toggled) {
+                Image(systemName: "minus")
+            } else {
+                Image(systemName: "plus")
+            }
+        }
+        .foregroundColor(.black)
+        .padding(8)
     }
 }
