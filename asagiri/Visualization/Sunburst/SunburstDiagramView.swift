3a6b3493f382b55fa8599550173f23555389fc46
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2023-11-29 13:37:40 +0100

    Minor changes for cleaning up as a poc

--- asagiri/Visualization/Sunburst/SunburstDiagramView.swift
+++ asagiri/Visualization/Sunburst/SunburstDiagramView.swift
@@ -386,7 +386,7 @@ struct SunburstDiagramView: View {
     var body: some View {
         ZStack {
             Chart([] as [LabelValuePair], id: \.self) { child in
-                SectorMark(angle: .value(Text(verbatim: child.label), child.value), innerRadius: .fixed(0), outerRadius: .fixed(0), angularInset: 2)
+                SectorMark(angle: .value(Text(verbatim: child.label), child.value), innerRadius: .fixed(0), outerRadius: .fixed(0), angularInset: 1)
                     .foregroundStyle(by: .value(Text(verbatim: child.label), child.label))
             }
             .chartForegroundStyleScale(domain: domains, range: chartColors)
@@ -394,7 +394,7 @@ struct SunburstDiagramView: View {
             .padding(16)
             ZStack {
                 Chart(generateLabelValuePairs(), id: \.self) { child in
-                    SectorMark(angle: .value(Text(verbatim: child.label), child.value), innerRadius: .fixed(62), outerRadius: .fixed(76), angularInset: 2)
+                    SectorMark(angle: .value(Text(verbatim: child.label), child.value), innerRadius: .fixed(62), outerRadius: .fixed(76), angularInset: 1)
                         .foregroundStyle(by: .value(Text(verbatim: child.label), child.label))
                 }
                 .chartForegroundStyleScale(domain: domains, range: chartColors)
@@ -456,7 +456,7 @@ struct SunburstDiagramView: View {
                 .chartLegend(.hidden)
                 Chart(generateLabelValuePairsLevel7(), id: \.self) { child in
                     if (child.label != "NA") {
-                        SectorMark(angle: .value(Text(verbatim: child.label), child.value), innerRadius: .fixed(154), outerRadius: .fixed(168), angularInset: 1)
+                        SectorMark(angle: .value(Text(verbatim: child.label), child.value), innerRadius: .fixed(156), outerRadius: .fixed(168), angularInset: 1)
                             .foregroundStyle(by: .value(Text(verbatim: child.label), child.label))
                     } else {
                         SectorMark(angle: .value(Text(verbatim: ""), child.value), innerRadius: .fixed(0), outerRadius: .fixed(0), angularInset: 2)
